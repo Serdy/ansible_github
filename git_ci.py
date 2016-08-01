9e4d5e684a22e60337a23169ad08c0603ae3f6d6
Oleksandr Serdiuk <oleksandr_serdiuk@epam.com>
2016-08-01 14:44:02 +0200

    add def

--- git_ci.py
+++ git_ci.py
@@ -3,7 +3,6 @@
 
 import param
 
-
 from github3 import GitHub
 import json
 
@@ -12,64 +11,64 @@ def initConnection():
     gh = GitHub(param.user_name, param.password)
     return gh
 
+
 def get_issuse():
     get_issuse = initConnection().issue(username=param.owner_rep, repository=param.repo, number=param.pull_req)
     return get_issuse
 
+
 def get_issuse_labels():
     get_issuse_labels = get_issuse().labels
-    # print json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-    # print json.dumps([{'inst_id': (get_issuse_labels())}])
     list_get_issuse_labels = []
-    # print type(get_issuse_labels())
-
     for label in get_issuse_labels():
         list_get_issuse_labels.append(str(label))
     print json.dumps({'labels': (list_get_issuse_labels)})
 
 
+def add_issuse_lable(labels):
+    issuse = get_issuse()
+    if isinstance(labels, list):
+        for label in labels:
+            issuse.add_labels(label)
+    elif isinstance(labels, str):
+        issuse.add_labels(labels)
+    get_issuse_labels()
+
+
+def remove_issuse_lable(labels):
+    issuse = get_issuse()
+    if isinstance(labels, list):
+        for label in labels:
+            issuse.remove_label(label)
+    elif isinstance(labels, str):
+        issuse.remove_label(labels)
+    get_issuse_labels()
+
+
+def get_pull_requests():
+    get_pull_requests = initConnection().pull_request(number=param.pull_req, owner=param.owner_rep,
+                                                      repository=param.repo)
+    return get_pull_requests
+
+
+def check_pull_requests_mergeable():
+    check_pull_requests_mergeable = get_pull_requests().mergeable
+    return check_pull_requests_mergeable
+
+def pull_requests_merge():
+    check_pull_requests_mergeable = get_pull_requests().mergeable
+    return check_pull_requests_mergeable
+
+def create_comment_issuse(comment):
+    issuse = get_issuse()
+    issuse.create_comment(comment)
 
 
 
-# get_issuse = g.issue(username=param.owner_rep,repository=param.repo,number=param.pull_req)
-# print dir(get_issuse)
-# print get_issuse_labels()
-# for v in get_issuse_labels():
-#     print v
 get_issuse_labels()
-# get_issuse.add_labels('bug')
-# get_issuse.remove_label('need CI tests')
-# print get_issuse.labels
-
-# Then play with your Github objects:
-# for repo in g.get_user().get_repos():
-#     print repo.name
-
-
-
-# def initConnection():
-#     gh = Github(login=param.user_name, password=param.password, repo=param.repo, user=param.owner_rep)
-#     return gh
-#
-# def get_lables_from_issuse():
-#     gh = initConnection()
-#     lables_from_issuse = gh.issues.labels.list_by_issue(number=param.pull_req)
-#     return lables_from_issuse
-#
-#
-# def pull_requests_mergeable():
-#     gh = initConnection()
-#     pr = gh.pull_requests.get(number=param.pull_req)
-#     return pr.mergeable
-#
-# def pull_requests_add_lable():
-#     gh = initConnection()
-#     lables_from_issuse = gh.issues.labels.add_to_issue(number=param.pull_req, labels=param.add_labla)
-#
-#
-# gh = Github(login=param.user_name, password=param.password, repo=param.repo, user=param.owner_rep)
-# pr = gh.pull_requests.get(number=param.pull_req)
-# print pr.mergeable
-# print dir(pr)
-# # gh.issues.labels.add_to_issue(number=param.pull_req, labels1='bug', labels2='invalid', user=param.user_name,repo=param.repo)
-# gh.issues.labels.add_to_issue(4, repo=param.repo, user=param.owner_rep, labels=('bug'))
+add_issuse_lable(labels=['invalid', 'bug'])
+remove_issuse_lable(labels=['invalid', 'bug'])
+print get_pull_requests().title
+print check_pull_requests_mergeable()
+# print pull_requests_merge()
+# print create_comment_issuse('test comment')
